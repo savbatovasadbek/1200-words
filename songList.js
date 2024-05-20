8928b61f61779e8fe29949ad819185ffc4b30b85
savbatovasadbek <asavbatov@gmail.com>
2024-05-20 12:46:56 +0500

    Data is finished

--- songList.js
+++ songList.js
@@ -1,6 +1,7 @@
 import days_of_the_week from "./assets/data/days_of_the_week.js";
 import months_of_the_year from "./assets/data/months_of_the_year.js";
+import subjects from "./assets/data/subjects.js";
 
-const songList = [...days_of_the_week, ...months_of_the_year];
+const songList = [...days_of_the_week, ...months_of_the_year, ...subjects];
 
 export default songList;
